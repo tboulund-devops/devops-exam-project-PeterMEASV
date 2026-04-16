281a206204168a724cac95082e7c38a88dd15170
Peteris Meiris <petmei01@easv365.dk>
2026-04-16 12:51:06 +0200

    fix: proxy API requests through nginx to bypass mixed content

--- client/src/baseUrl.ts
+++ client/src/baseUrl.ts
@@ -2,7 +2,7 @@
 import {forceLogout, TOKEN_KEY, tokenStorage} from "./Token.tsx";
 
 const isProduction = import.meta.env.PROD;
-const prod = "http://5.189.151.230:8080";
+const prod = "";
 const dev = "http://localhost:5107";
 
 const customFetch = async (url: RequestInfo, init?: RequestInit) => {
